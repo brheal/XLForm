27f1f8da48252c26fbfe5636f9e0a93bbdb66d81
Bryan Rhea <brheal@gmail.com>
2014-09-15 22:10:03 -0400

    add again

---
+++ XLForm/XL/Cell/XLFormDaysOfWeekCell.h
@@ -0,0 +1,13 @@
+//
+//  XLFormDaysOfWeekCell.h
+//  XLForm
+//
+//  Created by Bryan Rhea on 9/15/14.
+//  Copyright (c) 2014 Xmartlabs. All rights reserved.
+//
+
+#import "XLFormBaseCell.h"
+
+@interface XLFormDaysOfWeekCell : XLFormBaseCell
+
+@end
---
+++ XLForm/XL/Cell/XLFormDaysOfWeekCell.m
@@ -0,0 +1,21 @@
+//
+//  XLFormDaysOfWeekCell.m
+//  XLForm
+//
+//  Created by Bryan Rhea on 9/15/14.
+//  Copyright (c) 2014 Xmartlabs. All rights reserved.
+//
+
+#import "XLFormDaysOfWeekCell.h"
+
+@implementation XLFormDaysOfWeekCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
